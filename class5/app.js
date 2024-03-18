5b3cfb0ba66f095e8397b0c1a7a7b8fcf74ca03c
jafarramadan <jafar.n3n3@gmail.com>
2024-03-18 18:47:16 +0000

    okk

--- class5/app.js
+++ class5/app.js
@@ -13,8 +13,47 @@ console.log("all names",allnames)
 const oneConstantVar=5;
 
 
+//popup box
+//1-alert
+alert("welcome");
 
 
-console.log();
+//2-prompt
+var promptResult=prompt("enter your name")
+
+//3-confirm box
+var confirmResult=confirm("are you sure",promptResult);
 
 
+
+//if statment
+if(name == "jafar"){
+    console.log("this is jafar");
+}else if(name == jafarrr){
+    console.log("the name is jafarrr")
+}
+
+//switch
+let dayOfweek=4;
+
+switch(dayOfweek){
+    case 1: console.log("mon");
+    break;
+    case 2: console.log("tuesday");
+    break;
+    case 3: console.log("wed");
+    break;
+    case 4: console.log("thur");
+    break;
+    case 5: console.log("fri");
+    break;
+    case 6: console.log("sat");
+    break;
+    case 7: console.log("sun");
+    break;
+}
+
+//object
+let myobj={first:"123",second:456,third:true}
+myobj.first;
+myobj["first"];
