fcf913eb9447759c08ef75d8cafafa480a15245c
jafarramadan <jafar.n3n3@gmail.com>
2024-03-18 08:38:57 +0000

    ok

---
+++ class5/app.js
@@ -0,0 +1,20 @@
+'use strict';
+var n1=10;
+var n2=20;
+var sum=n1+n2;
+console.log("sum of numbers",sum);
+
+
+var name="jafar";
+var name2='t';
+var allnames=name+" "+name2;
+
+console.log("all names",allnames)
+const oneConstantVar=5;
+
+
+
+
+console.log();
+
+
