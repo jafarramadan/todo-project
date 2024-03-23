a7a723b6420a5d93e0ef5db104fc7a12027df119
jafarramadan <jafar.n3n3@gmail.com>
2024-03-23 18:41:44 +0000

    adding three questions

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-let userName = prompt("Please enter your name:");
+/* let userName = prompt("Please enter your name:");
 let userGender = prompt("Please enter your gender (male or female):");
 
 let userAge = prompt("Please enter your age:");
 if(userAge<=0){
     alert("your age is less than 0");
 }
+
 
 let skipWelcome = confirm("Do you want to skip the welcoming message?");
 
@@ -18,3 +19,34 @@ if (!skipWelcome) {
     alert("wrong");
 }
 }
+
+ */
+
+
+
+/*=========adding the 3 questions==================================================*/
+
+ 'use strict'
+var answers = [];
+let answer;
+function qustionsPopUp() {
+    let arrayOfQuestions = ["Do you like swimming ", "Do you like archery", "Do you like horse riding"];
+    for (let i = 0; i < arrayOfQuestions.length; i++) {
+        answer = prompt(arrayOfQuestions[i]);
+        while(!answer) {
+            if (!answer) {
+                alert("invalid");
+            }
+            answer = prompt(arrayOfQuestions[i]);
+        }
+        answers.push(answer);
+    }
+    return answers;
+} 
+
+qustionsPopUp();
+console.log("final answers : " + answers); 
+
+
+
+
